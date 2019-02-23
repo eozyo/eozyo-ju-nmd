593c561ec267f48ef9690123950ef7cee1e06c0f
EOZyo <apache@eozyo.com>
2019-02-23 16:09:05 +0100

    closing a04

--- a04/scripts.js
+++ a04/scripts.js
@@ -10,16 +10,13 @@ window.onload = function() {
 	];
 	loadList(items);
 }
-
 function loadList(items) {
-	console.log("Program started.\nLoading products…");
-	//debugger;
 	var ul = document.querySelector("#orders ul:first-of-type");	
 	var li,span;
 	for(var i=0; i<items.length;i++) {
 		span				= document.createElement("span");
 		span.id 			= "product-" + i;
-		span.className		= "tag product-" + i;
+		span.className		= "tag " + span.id;
 		span.innerHTML		= items[i].name + " | " + items[i].price + ":-";
 		span.dataset.name	= items[i].name;
 		span.dataset.price  = items[i].price;
@@ -28,48 +25,29 @@ function loadList(items) {
 		li.appendChild(span);
 		ul.appendChild(li);
 	}
-	console.log("Rutine ended. Products loaded.")
-	console.log(ul);
 }
-
-function addOrder(item) {
-	//debugger;
+function addOrder(items) {
 	var ul 			= document.querySelector("#orders p + ul")
-	var li			= item.cloneNode(true);
+	var li			= items.cloneNode(true);
 	var span		= li.firstChild;
-	var subtotal	= document.querySelector("#orders p:first-of-type span");
+	var span_sale	= new TrackIncome(document.querySelector("#orders p:first-of-type span"),span.dataset.price);
 	var obutton		= document.querySelector("#orders button");
-	span.className	= span.id + " tag";
-	span.innerHTML	= span.dataset.name + " | X";
-	span.onclick	= function () { 
-		//debugger;
-		var removal = parseInt(subtotal.innerHTML) - parseInt(span.dataset.price);
-		if(removal>0) { 
-			subtotal.innerHTML = removal; 
-		} else { 
-			subtotal.remove();
-			obutton.setAttribute("disabled","disabled");
-		}
-		this.parentElement.remove();
-	}
+	span.innerHTML	= span.dataset.name + ' <span class="remove-tag">×</span>';
+	span.onclick	= function () { removeOrder(this,(parseInt(span_sale.elm.innerHTML) - parseInt(span.dataset.price)),span_sale,obutton); }
+
 	span.removeAttribute("id");
 	ul.appendChild(li);
-	if(!subtotal) { 
-		subtotal = document.createElement("span");
-		subtotal.innerHTML = span.dataset.price;
-		document.querySelector("#orders p:first-of-type").appendChild(subtotal);
-	} else {
-		subtotal.innerHTML = parseInt(subtotal.innerHTML) + parseInt(span.dataset.price); 
-	}
 	if(obutton.hasAttribute("disabled")) { 
 		obutton.removeAttribute("disabled");
-		var totalOrdered = new Counter("#totalOrdered");
-		obutton.onclick = function() { sendToBarista(ul,subtotal); totalOrdered.addition(subtotal.innerHTML); } 
+		obutton.onclick = function() { span_sale.record(ul.children); sendToBarista(ul,span_sale); } 
 	}
 }
-
-function sendToBarista(ul,subtotal) {
-	//debugger;
+function removeOrder(span,residual,span_sale,obutton) {
+	if(residual>0) { span_sale.elm.innerHTML = residual; }
+	else { span_sale.elm.remove(); obutton.setAttribute("disabled","disabled"); }
+	span.parentElement.remove();
+}
+function sendToBarista(ul,span_sale) {
 	document.querySelector("#orders button").setAttribute("disabled","disabled");
 	var bbutton = document.querySelector("#wip button");
 	var span;
@@ -84,71 +62,87 @@ function sendToBarista(ul,subtotal) {
 		bbutton.removeAttribute("disabled"); 
 		bbutton.onclick = function() { prepareDrink(this); }
 	}
-	subtotal.remove();
+	span_sale.elm.remove();
 }
-
 function prepareDrink(bbutton) {
-	//debugger;
-	var ul	= document.querySelector("#wip p + ul");
-	if(ul.childElementCount>0) { 
-		document.querySelector("#wip ul:first-of-type").appendChild(ul.firstChild);
-		bbutton.innerHTML = "Serve beverage...";
-		bbutton.onclick = function() { serveBeverage(this); }
-	} else { console.log("function prepareDrink(else)"); }
-}
-
-function serveBeverage(bbutton) {
-	var ul  = document.querySelector("#wip p + ul");
-	var wip = document.querySelector("#wip ul:first-of-type");
-	finish(wip.firstChild);
-	bbutton.innerHTML = "Prepare beverage";
-	bbutton.onclick	= function() { prepareDrink(this); } 
-	if(ul.childElementCount<=0) { 
-		ul.innerHTML= '<li id="tempLI"><span class="tag">Up to date</li>';
-		bbutton.setAttribute("disabled","disabled"); 
+	var ul		= document.querySelector("#wip p + ul");
+	var ul_qb	= document.querySelector("#wip ul:first-of-type");
+	switch(true) {
+		case (ul.childElementCount  > 0 && ul_qb.childElementCount == 0):
+			ul_qb.appendChild(ul.firstChild);
+			bbutton.innerHTML = "Serve beverage…";
+		break;
+		case (ul.childElementCount >= 0 && ul_qb.childElementCount == 1):
+			finish(ul_qb.firstChild);
+			if (ul.childElementCount > 0) {
+				bbutton.innerHTML = "Prepare next beverage…";
+			}
+			if(ul.childElementCount == 0 && ul_qb.childElementCount==0 ) {
+				ul.innerHTML= '<li id="tempLI"><span class="tag">Up2date</li>';
+				bbutton.innerHTML = "Prepare beverage";
+				bbutton.setAttribute("disabled","disabled");
+			}
+		break;
 	}
 }
-
-function Counter(el) {
-	//debugger;
-	var elm = document.querySelector(el);
-	this.value		= (isNaN(parseInt(elm.innerHTML)) ? 0 : parseInt(elm.innerHTML));
-	this.addition	= function(orderValue) {
-		this.value = parseInt(orderValue) + this.value;
-		elm.innerHTML = this.value;
-	}
-}
-
-function finish(product) {
-	//debugger;
-	var finished = document.getElementById("finished");
-	var finishedItem;
+function finish(li) {
+	var finished = document.getElementById("finished");	
 	if(finished.childElementCount==0){
-		product.firstChild.dataset.count=1;
-		product.firstChild.setAttribute("title", "1");
-		finished.appendChild(product);
+		li.firstChild.dataset.count=1;
+		li.firstChild.title=1;
+		finished.appendChild(li);
 	} else {
-		for(var i = 0; i<finished.childElementCount;i++){
-			finishedItem = finished.children[i];
-			if (product.firstChild.dataset.name == finishedItem.firstChild.dataset.name) {
-				var match = finishedItem;
-				break;
+		for(var i=0;i<finished.childElementCount;i++){
+			var finishedItem = finished.children[i];
+			if(li.firstChild.dataset.name == finishedItem.firstChild.dataset.name) {
+				var match = finishedItem; break;
 			} 
 		}	
 		if(match) {
 			match.firstChild.dataset.count++;
 			match.firstChild.title = match.firstChild.dataset.count;
-			product.remove();
+			li.remove();
 		} else {
-			product.firstChild.dataset.count=1;
-			product.firstChild.title=1;
-			finished.appendChild(product);
+			li.firstChild.dataset.count=1;
+			li.firstChild.title=1;
+			finished.appendChild(li);
 		}
 	}
 }
-
-
-
-
-
-
+function TrackIncome($el,$amt) {
+	var sales = document.getElementById("sales");
+	if($el){ $el.innerHTML = parseInt($el.innerHTML) + parseInt($amt); } 
+	else {
+		$el = document.createElement("span"); $el.innerHTML = $amt;
+		document.querySelector("#orders p:first-of-type").appendChild($el);
+	}
+	this.elm 			= $el;
+	this.currentTotal 	= (isNaN(parseInt(sales.innerHTML)) ? 0 : parseInt(sales.innerHTML));
+	this.record 		= function(items) { 
+		sales.innerHTML 	= (this.currentTotal + parseInt($el.innerHTML));
+		sales.style.display	= "inline";
+		// saves the sales into a global object "summary"
+		var order = { items : [], total : null, };
+		for(var i=0;i<items.length;i++) {
+			order.items[i] = { name : items[i].firstChild.dataset.name, price : +items[i].firstChild.dataset.price }
+			order.total = +order.total + order.items[i].price;
+		}	
+		// Call "summary" to see all the available data.
+		if(window.summary) {
+			summary.totalOrders++;
+			summary.orders.push({ items: order.items, total: +order.total});
+			summary.totalItems = summary.totalItems + order.items.length;
+			summary.total		 = summary.total + order.total;
+		} else {
+			window.summary = {
+				totalOrders : 1,
+				totalItems	: +order.items.length,
+				total		: +order.total,
+				orders		: [{ items: order.items, total: order.total}]
+			}
+		}
+	}
+}
+/* Helper functions */
+function _random($lowerLimit,$higherLimit) { var random = Math.floor((Math.random()*$higherLimit)+$lowerLimit); return random; }
+function _(str) { console.log(str); }
