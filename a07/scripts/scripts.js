3c3f478f63a416ddcb580c2778cb29a5b91ed777
EOZyo <apache@eozyo.com>
2019-02-23 22:57:33 +0100

    folder 07 and file system set up

---
+++ a07/scripts/scripts.js
@@ -0,0 +1,10 @@
+/**
+*
+*  title	: JavaScript Lab 07, Client-side Programming I && II
+*  author	: orem18kz
+*  email	: orem18kz[@]student,ju,se
+*
+*/
+
+/* Helper functions */
+function _(str) { console.log(str); }
