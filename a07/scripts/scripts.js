43ad64e0a8432d251303d61b8b958684cd727b87
EOZyo <apache@eozyo.com>
2019-03-16 19:40:41 +0100

    small fix

--- a07/scripts/scripts.js
+++ a07/scripts/scripts.js
@@ -9,7 +9,7 @@ var guessTheNumber;
 
 window.onload = function() {
 	guessTheNumber = new GuessGame();
-	["keypress","input","paste"].forEach(function($event) { guessTheNumber.html.input.addEventListener($event, function(e) { guessTheNumber.input(e); }) }
+	["keypress","input","paste"].forEach(function($event) { guessTheNumber.html.input.addEventListener($event, function(e) { guessTheNumber.input(e); }) });
 	guessTheNumber.html.input.addEventListener( "select", function(e) { guessTheNumber.erase(e); guessTheNumber.draw(); });
 	guessTheNumber.html.input.setAttribute("min",guessTheNumber.data.lowLimit);
 	guessTheNumber.html.input.setAttribute("max",guessTheNumber.data.highLimit);
@@ -96,76 +96,6 @@ function GuessGame($inputTag="inputTag",$buttonTag="validate",$guessArea="guessA
 		}
 	}
 
-	// inputs
-/*this.input			= function(e) {
-		this.data.keyPressed		= +e.charCode;
-		let that								= this;
-		let valuePressed				= this.data.validKeys.indexOf(this.data.keyPressed);
-		let current_input				= { value : this.html.input.value, charCount : this.html.input.value.length }
-		let sum, err_msg;
-		if( !isNaN(this.data.keyPressed) && this.data.validKeys.includes(this.data.keyPressed) ) {
-			_("Key pressed: " + this.data.keyPressed + ": valid : " + valuePressed);
-			_("current current_input.charCount:" + current_input.charCount)
-
-			sum 		= current_input.value;
-			err_msg = " would not be ";
-
-			sum += valuePressed;
-			sum = parseInt(sum);
-
-			if( sum>(this.data.lowLimit-1) && sum<(this.data.highLimit+1) ) {
-				if(this.html.button.hasAttribute("disabled"))	{ this.html.button.removeAttribute("disabled"); }
-
-				for(var i=0;i<this.html.ul.children.length;i++) {
-					if(i<sum) { this.html.ul.children[i].className="guess"; }
-					else 			{ this.html.ul.children[i].classList.remove("guess"); }
-				}
-				this.html.button.onclick = function(e) { that.validate(); }
-			}
-			else {
-				e.preventDefault();
-
-				if(!isNaN(this.data.keyPressed) && isNaN(sum)) {
-					err_msg = "Something does not seem quite right";
-					this.html.input.value="";
-				} else {
-					switch (true) {
-						case sum<this.data.lowLimit:
-							err_msg += "higher or equal to the low limit of " + this.data.lowLimit;
-							break;
-						case sum>this.data.highLimit:
-							err_msg += "lower or equal to the high limit of " + this.data.highLimit;
-							break;
-						default:
-
-					}
-					err_msg = "Sorry, " + sum + err_msg;
-				}
-				alert(err_msg +".\nPlease try again.");
-				this.html.input.setSelectionRange(0,this.html.input.value.length);
-			}
-
-		} else {
-			e.preventDefault();
-			alert("You can only enter numbers. Thanks.")
-		}
-
-
-	}
-	// outputs
-	this.output			= function(e) {
-		_("this.output()")
-		let no_val = (this.html.input.value=="") ? true : false;
-		let curVal = (this.html.input.value!="") ? +this.html.input.value : 0;
-		_(e);
-		if( curVal<(this.data.lowLimit-1) || curVal>(this.data.highLimit+1) || no_val===true ) {
-			this.html.button.setAttribute("disabled","disabled");
-		}
-		if(e.keyCode==8 || e.keyCode==46) {
-			this.resetC();
-		}
-	}*/
-	// erases
 	this.erase			= function(e) {
 		e.preventDefault();
 		this.html.input.value="";
